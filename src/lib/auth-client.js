bdae2047e4eee1684229b72e722b0bfda7f82e4f
mahemel <mahbubhasanhemel@gmail.com>
2026-04-28 06:17:38 +0600

    better auth implemented

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "https://mahemel-online-news-portal.vercel.app/"
+    baseURL: process.env.BETTER_AUTH_URL
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
